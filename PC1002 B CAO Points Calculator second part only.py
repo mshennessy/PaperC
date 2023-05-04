ab535027df02198ef40e0f244a28d62d97f42c4a
mshennessy <112807189+mshennessy@users.noreply.github.com>
2023-05-04 13:36:26 +0100

    Add files via upload

---
+++ PC1002 B CAO Points Calculator second part only.py
@@ -0,0 +1,16 @@
+# GH 1002 CAO Points calculator parts f and g only
+# Student No (or name): ________________
+
+
+print("Welcome to my CAO points calculator")
+
+#Skip over the whole business of inputting the data
+#Work from these lists
+
+subjects=['Maths','English','Irish','German','Comp Sci','Physics','Art','Music']
+levels=['H','H','H','O','H','O','H','H']
+grades=[2,5,2,4,3,5,3,2]
+points=[105,56,88,28,77,20,77,88]
+
+#Use these to answer the parts f and g
+#Or use your one code from earlier
