5405251cb5c135a70e8d78f515121957c76b8349
mshennessy <112807189+mshennessy@users.noreply.github.com>
2023-05-15 09:11:20 +0100

    Add files via upload

---
+++ PC1601 Averages.py
@@ -0,0 +1,20 @@
+# GH 1601 Averages
+# Student No (or name): ________________
+
+def findMean(values):
+    
+    mean = 0
+    return mean
+
+
+numList = [0,1,2,3,2,3,4,8,1,9]
+# Test Data
+#numList = [22,34,65,25,87,43,56,98,54,43,21,54,88,17,66,32,87,59,43,12,88,63,28,75]
+# numList = [2.3,4.2,5.6,1.7]
+
+myMean = findMean(numList)
+
+print("Welcome to the averages calculator")
+
+print("The mean of the values is",myMean)
+
