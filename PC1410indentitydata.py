b43954c0f623d8e52766e7ba9955ef4109967fdb
mshennessy <112807189+mshennessy@users.noreply.github.com>
2023-05-17 11:20:28 +0100

    Add files via upload

---
+++ PC1410indentitydata.py
@@ -0,0 +1,8 @@
+# PC1410 Identity data 
+# Name and School: 
+
+print("Enter your name, email address and phone number")
+
+
+firstName = input("Enter your first name : ")
+surName = input("Enter your surname : ")
