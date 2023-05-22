7b329932eab86d4fc0701dbde12bd1f5d40b09cf
mshennessy <112807189+mshennessy@users.noreply.github.com>
2023-05-22 17:30:19 +0100

    Add files via upload

---
+++ 5th Year Summer Exam 2023 q16a.py
@@ -0,0 +1,8 @@
+# Paper C Summer Exam 5th year
+# Student name: _______
+# Analyse a word input by the user
+# Part a)
+
+vowels=['a','e','i','o','u']
+
+word = input("Please type in a word : ")
---
+++ 5th Year Summer Exam 2023 q16b.py
@@ -0,0 +1,11 @@
+# Paper C Summer Exam 5th year
+# Student name: _______
+# Guessing Game
+# Part b)
+
+import random
+
+diceRoll=random.randint(1,6)
+
+diceGuess = input("Guess which number was rolled")
+
